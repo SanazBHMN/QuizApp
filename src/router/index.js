212ddc4173f09e75cb62a2562271b9fbc706d7ac
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-12-11 13:45:36 +0330

    Define /quiz route

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+// define router rules
+import { createRouter, createWebHistory } from "vue-router";
+import QuizesView from "../views/QuizesView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "quizes",
+      component: QuizesView,
+    },
+    {
+      path: "",
+      name: "",
+    },
+  ],
+});
+
+export default router;
